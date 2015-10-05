70d2b5cf8332da9589e77fbff13f3a51153cb10d
tbrenneison <tori.brenneison@gmail.com>
2015-10-05 13:50:30 -0400

    Monday 10/05/15

---
+++ stackImplementation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a program which implements a stack interface for integers. The interface should have ‘push’ and ‘pop’ functions. 
+            Your task is to ‘push’ a series of integers and then ‘pop’ and print every alternate integer.
+            Print to stdout every alternate space delimited integer, one per line.
+            */
+
+            string line = "1 2 3 4"; //test input
+            line.Trim();
+            string[] splitLine = line.Split(' ');
+
+            Stack<string> nums = new Stack<string>();
+            foreach (string str in splitLine)
+            {
+                nums.Push(str);
+            }
+
+            while (nums.Count() > 0)
+            {
+                Console.Write(nums.Pop() + " ");
+                if (nums.Count != 0)
+                    { nums.Pop(); }
+
+            }
+        }
+    }
+}
---
+++ validParentheses/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace validParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Given a string comprising just of the characters (,),{,},[,] determine if it is well-formed or not.
+            //Each line in this file contains a string comprising of the characters mentioned above.
+            //Print out True or False if the string is well-formed.
+
+            //if opening bracket, push to stack
+            //if closing bracket, peek stack and see if top char is matching opening bracket 
+            //if yes, pop stack 
+            //if stack count = 0, brackets are well-formed 
+
+            string line = "([)]";//test input (false)
+
+            char[] chars = line.ToCharArray();
+            Stack<char> charStack = new Stack<char>();
+
+            foreach(char c in chars) //consider each character left-right 
+            {
+                if (c == '(' || c == '[' || c == '{') //push opening brackets to stack
+                {
+                    charStack.Push(c);
+                }
+                else if (charStack.Count() > 0) //compare closing brackets to top of stack to see if they match opening bracket
+                {
+                    char x = c;
+                    char y = charStack.Peek();
+                    switch (x) //pop the stack if the brackets close up
+                    {
+                        case ')':
+                            if (y == '(')
+                            { charStack.Pop(); }
+                            break;
+                        case ']':
+                            if (y == '[')
+                            { charStack.Pop(); }
+                            break;
+                        case '}':
+                            if (y == '{')
+                            { charStack.Pop(); }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else//if first bracket is a closing bracket 
+                {
+                    continue;
+                }
+            }
+
+            //if the stack is empty, return true 
+            if(charStack.Count > 0) 
+            { Console.WriteLine("False"); }
+            else
+            { Console.WriteLine("True"); }
+
+            
+
+        }
+    }
+}
